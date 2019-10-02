7ef448cd6ffe05f011519d94aff65d3376603af5
squirmlabs <squirmlabs@gmail.com>
2019-10-01 17:18:43 -0700

    Add lines to Battery hook

--- src/Battery/Battery.hook.js
+++ src/Battery/Battery.hook.js
@@ -3,6 +3,7 @@ import Battery from './Battery';
 
 const useBattery = () => {
   const [battery, setBattery] = useState({ level: 0, charging: false });
+  
   const handleChange = ({ target: { level, charging } }) =>
     setBattery({
       level,
@@ -11,16 +12,19 @@ const useBattery = () => {
 
   useEffect(() => {
     let battery;
+
     navigator.getBattery().then(bat => {
       battery = bat;
       battery.addEventListener('levelchange', handleChange);
       battery.addEventListener('chargingchange', handleChange);
       handleChange({ target: battery });
     });
+
     return () => {
       battery.removeEventListener('levelchange', handleChange);
       battery.removeEventListener('chargingchange', handleChange);
     };
+
   }, []);
   return battery;
 };
